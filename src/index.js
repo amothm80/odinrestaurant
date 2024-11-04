653d18481efbaed3f02a9c361e97a666d88fa022
amothm80 <amo.1980@gmail.com>
2024-11-04 17:28:50 +0300

    HTML and CSS

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import { greeting } from './greeting.js';
+
+// console.log(greeting);
+// const para = document.createElement('h1');
+// const node = document.createTextNode(greeting);
+// para.appendChild(node);
+
+// const elements = document.getElementsByTagName('body');
+// for (const el of elements) {
+//   el.appendChild(para);
+// }
